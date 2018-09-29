f4a3a3e0ca1771543e8ac29527f911b085e705e1
ankitsahitya <ankit@gkmit.co>
2018-09-29 17:48:07 +0530

    migrations updated

--- db/migrate/20180929110812_create_audis.rb
+++ db/migrate/20180929110812_create_audis.rb
@@ -4,7 +4,7 @@ class CreateAudis < ActiveRecord::Migration[5.2]
       t.integer :number
       t.integer :no_of_seats
       t.integer :price_of_seat
-      t.integer :theatre_id
+      t.references :theater, index: true, foreign_key: true
     end
   end
 end
